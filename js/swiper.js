eea0a775695848f985c83d7761126afa92296afe
9ouo9pp <9ouo9pp@gmail.com>
2025-08-09 11:36:30 +0900

    slide 수정

--- js/swiper.js
+++ js/swiper.js
@@ -5,7 +5,7 @@ var swiper = new Swiper(".mySwiper", {
   spaceBetween: 30,
   centeredSlides: true,
   autoplay: {
-    delay: 3500,
+    delay: 4000,
     disableOnInteraction: false
   },
   pagination: {
